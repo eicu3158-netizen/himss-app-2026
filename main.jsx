e0bad88a114b8f65f649642f41f68fc97a127005
eicu3158-netizen <eicu3158@gmail.com>
2026-03-06 22:55:41 +0800

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// 修正：必須指向與 Canvas 中主程式一致的檔名 App.jsx
 import App from './App.jsx'
 
+// 將 React 內容掛載到 index.html 中的 <div id="root"></div>
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-
 )
